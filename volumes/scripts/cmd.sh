f6bd1a293a5c3020deefc7cb2fc111f001b769aa
sunzhouxingjay <454951911@qq.com>
2023-10-28 18:49:51 +0800

    images

--- volumes/scripts/cmd.sh
+++ volumes/scripts/cmd.sh
@@ -2,7 +2,7 @@
 service mysqld start && \
 cd /home/sunweekstar/redis && \
 redis-server redis.conf && \
-. /usr/local/activitiStart.sh && \
+. /usr/local/scripts/wfServiceScripts/activitiStart.sh && \
 . /usr/local/scripts/ipfsScripts/ipfsStart.sh && \
 . /usr/local/scripts/nacosScripts/createNacosConfig.sh && \
 peer node start
